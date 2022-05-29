7f5fd34162f734bfb95cee198d913237716e71c3
shahsaaqib <shahsaaqib01@gmail.com>
2022-05-29 18:52:42 +0530

    post done

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,3 +1,5 @@
+const Bootcamp = require('../models/Bootcamp');
+
 // @desc    Get all Bootcamps
 // @route   GET /api/v1/bootcamps
 // @access  Public
@@ -18,8 +20,16 @@ exports.getBootcamp = (req, res, next) => {
 // @route   POST /api/v1/bootcamps
 // @access  Private
 
-exports.createBootcamp = (req, res, next) => {
-  res.status(200).json({ success: true, msg: 'create a bootcamp' });
+exports.createBootcamp = async (req, res, next) => {
+  try {
+    const bootcamp = await Bootcamp.create(req.body);
+    res.status(201).json({
+      success: true,
+      data: bootcamp,
+    });
+  } catch (error) {
+    res.status(400).json({ success: false, errormsg: error });
+  }
 };
 
 // @desc    Update Bootcamp
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
+const colors = require('colors');
 const connectDB = require('./config/db');
 
 // load env variables
@@ -14,6 +15,9 @@ const bootcamps = require('./routes/bootcamps');
 
 const app = express();
 
+//body parser
+app.use(express.json());
+
 //Dev logging middleware
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
@@ -24,9 +28,18 @@ app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(
+const server = app.listen(
   PORT,
   console.log(
-    `Server running in ${process.env.NODE_ENV} mode on port : ${PORT}`
+    `Server running in ${process.env.NODE_ENV} mode on port : ${PORT}`.yellow
+      .bold
   )
 );
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`.red);
+
+  //close sever and exit process
+  server.close(() => process.exit(1));
+});

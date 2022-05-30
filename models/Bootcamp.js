97414a7818bb107d35242ef8ca1359ce7803052e
shahsaaqib <shahsaaqib01@gmail.com>
2022-05-30 11:42:39 +0530

    middleware Geocoder & slugify

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
-//const slugify = require('slugify');
-//const geocoder = require('../utils/geocoder');
+const slugify = require('slugify');
+const geocoder = require('../utils/geocoder');
 
 const BootcampSchema = new mongoose.Schema({
   name: {
@@ -110,15 +110,15 @@ const BootcampSchema = new mongoose.Schema({
     toObject: { virtuals: true }
   
 );
-
+*/
 // Create bootcamp slug from the name
-BootcampSchema.pre('save', function(next) {
+BootcampSchema.pre('save', function (next) {
   this.slug = slugify(this.name, { lower: true });
   next();
 });
 
 // Geocode & create location field
-BootcampSchema.pre('save', async function(next) {
+BootcampSchema.pre('save', async function (next) {
   const loc = await geocoder.geocode(this.address);
   this.location = {
     type: 'Point',
@@ -128,14 +128,14 @@ BootcampSchema.pre('save', async function(next) {
     city: loc[0].city,
     state: loc[0].stateCode,
     zipcode: loc[0].zipcode,
-    country: loc[0].countryCode
+    country: loc[0].countryCode,
   };
 
   // Do not save address in DB
   this.address = undefined;
   next();
 });
-
+/*
 // Cascade delete courses when a bootcamp is deleted
 BootcampSchema.pre('remove', async function(next) {
   console.log(`Courses being removed from bootcamp ${this._id}`);

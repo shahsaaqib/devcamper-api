567e738edf0f52396ed0bd5bc127f1e045076f6f
shahsaaqib <shahsaaqib01@gmail.com>
2022-06-01 13:20:47 +0530

    forgot password

--- controllers/courses.js
+++ controllers/courses.js
@@ -48,11 +48,23 @@ exports.getCourse = asyncHandler(async (req, res, next) => {
 
 exports.AddCourse = asyncHandler(async (req, res, next) => {
   req.body.bootcamp = req.params.bootcampId;
+  req.body.user = req.user.id;
+
   const bootcamp = await Bootcamp.findById(req.params.bootcampId);
 
   if (!bootcamp) {
     return next(
       new errorResponse(`No bootcamp with the id of ${req.params.bootcampId}`)
+    );
+  }
+
+  // Make sure user is Bootcamp owner
+  if (bootcamp.user.toString() !== req.user.id && req.user.role !== 'admin') {
+    return next(
+      new errorResponse(
+        `User '${req.user.id}' is not authorized to add a course to bootcamp '${bootcamp._id}'`,
+        401
+      )
     );
   }
 
@@ -69,15 +81,29 @@ exports.AddCourse = asyncHandler(async (req, res, next) => {
 // @access  Private
 
 exports.updateCourse = asyncHandler(async (req, res, next) => {
-  const course = await Course.findByIdAndUpdate(req.params.id, req.body, {
-    new: true,
-    runValidators: true,
-  });
+  let course = await Course.findByIdAndUpdate(req.params.id);
+
   if (!course) {
     return next(
       new errorResponse(`Course not found with id of ${req.params.id}`, 404)
     );
   }
+
+  // Make sure user is Bootcamp owner
+  if (course.user.toString() !== req.user.id && req.user.role !== 'admin') {
+    return next(
+      new errorResponse(
+        `User '${req.user.id}' is not authorized to update this course.`,
+        401
+      )
+    );
+  }
+
+  course = await Course.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
+
   res.status(200).json({
     success: true,
     data: course,
@@ -93,6 +119,16 @@ exports.deleteCourse = asyncHandler(async (req, res, next) => {
   if (!course) {
     return next(
       new errorResponse(`Course not found with id of ${req.params.id}`, 404)
+    );
+  }
+
+  // Make sure user is Bootcamp owner
+  if (course.user.toString() !== req.user.id && req.user.role !== 'admin') {
+    return next(
+      new errorResponse(
+        `User '${req.user.id}' is not authorized to delete this course.`,
+        401
+      )
     );
   }
 
--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,15 @@
 const express = require('express');
-const { register, login, getMe } = require('../controllers/auth');
+const {
+  register,
+  login,
+  getMe,
+  forgotPassword,
+} = require('../controllers/auth');
 const { protect } = require('../middleware/auth');
 const router = express.Router();
 
 router.post('/register', register);
 router.post('/login', login);
+router.post('/forgotpassword', forgotPassword);
 router.get('/me', protect, getMe);
 module.exports = router;

30f85f7d58b3baf06378db874d0ef2f323fedd30
shahsaaqib <shahsaaqib01@gmail.com>
2022-06-02 11:24:54 +0530

    backend done

--- server.js
+++ server.js
@@ -7,6 +7,9 @@ const cookieParser = require('cookie-parser');
 const mongoSanitize = require('express-mongo-sanitize');
 const helmet = require('helmet');
 const xss = require('xss-clean');
+const rateLimit = require('express-rate-limit');
+const hpp = require('hpp');
+const cors = require('cors');
 const errorHandler = require('./middleware/error');
 const connectDB = require('./config/db');
 const fileupload = require('express-fileupload');
@@ -48,6 +51,20 @@ app.use(helmet());
 
 // Prevent xss attack
 app.use(xss());
+
+//Rate limiting
+const limiter = rateLimit({
+  windowMs: 10 * 60 * 1000, //10 mins.
+  max: 100,
+});
+
+app.use(limiter);
+
+// prevent Http param pollution
+app.use(hpp());
+
+//Enable CORS
+app.use(cors());
 
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
